1569edad41365a9090e728528812d1b055acaa1a
Gayathri H <gayathrih.cse@skit.org.in>
2024-10-18 20:20:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -170,5 +170,4 @@ def merge_similar_tables(tables):
     return combined_tables
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
